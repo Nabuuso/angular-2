c3e487214613742742f612b06b53400cfc27c876
betty <betty.nabuuso@moringaschool.com>
2021-10-16 10:46:08 +0300

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { StudentService } from '../app/services/student.service'
 
 @NgModule({
   declarations: [
@@ -9,8 +9,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    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
   ],
-  providers: [],
+  providers: [StudentService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  cars = [
+    {name: "toyota", colour: "white"},
+    {name: "benz", colour: "blue"},
+    {name: "suzuki", colour: "silver"}
+  ]
+}
---
+++ src/app/carlist.ts
@@ -0,0 +1,8 @@
+export class Carlist {
+}
+export let cars: Carlist[] = [
+    new cars(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2021,9,14)),
+    new cars(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2021,6,9)),
+    new cars(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2021,1,12)),
+   
+  ]
