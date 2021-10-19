58ee7f5239e23862997e1c1c96a9622157ab174a
betty <betty.nabuuso@moringaschool.com>
2021-10-19 04:48:41 +0300

    file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,49 +10,39 @@ import { UsersService } from './service/users/users.service';
 
 export class AppComponent implements OnInit {
   title = 'Car-app';
-
   username:string = 'Nabuuso'
-  
   repositories: any[] =[]
-  getPublicRepositories: any;
   constructor(private usersService:UsersService){}
     ngOnInit(): void {   
-      
       this.getPublicRepositories();
     }
     
-    // getPublicRepositories(){
-    //   this.usersService.getUsers('this.Nabuuso').subscribe;((response: any[])=>{
-    //     console.log(response); 
-    //     this.repositories = response;
-    //   })
+    getPublicRepositories(){
+      this.usersService.getUsers(this.username).then<any[]>(response) =>{
+        console.log(response); 
+        this.repositories = response;
+      }
+    }
+  }
+  ngOnInit(); void {
+    throw: new Error('Method not implemented.'),
+  }
+  ngOnInit(); void {
+    throw: new Error('Method not implemented.'),
+  }
 
+  ngOnInit(); void {
+    throw: new Error('Method not implemented.'),
+  }
+function ngOnInit() {
+  throw new Error('Function not implemented.')
+}
 
-    this.usersService.getUsers(this.username).then((response: any)=>{
-      console.log(response);
-      this.repositories = response;
-  });
-  
+function getPublicRepositories() {
+  throw new Error('Function not implemented.');
+}
 
-//   ngOnInit(); void {
-//     throw: new Error('Method not implemented.'),
-//   }
-//   ngOnInit(); void {
-//     throw: new Error('Method not implemented.'),
-//   }
-
-//   ngOnInit(); void {
-//     throw: new Error('Method not implemented.'),
-//   }
-// function ngOnInit() {
-//   throw new Error('Function not implemented.')
-// }
-
-// function getPublicRepositories() {
-//   throw new Error('Function not implemented.');
-// }
-
-// function subscribe(_arg0: (response: any) => any): any {
-//   throw new Error('Function not implemented.');
-// }
+function subscribe(_arg0: (response: any) => any): any {
+  throw new Error('Function not implemented.');
+}
 
