bf74741010df7acb49dcca04974a66b03e754af6
betty <betty.nabuuso@moringaschool.com>
2021-10-20 21:48:04 +0300

    adding css

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL: 'https://api.github.com',
+  Apikey: "57057da0b608d813bd90e470b5d3f271bbd42241",
+  ClientId: "Iv1.00a4d777747185b6"
 };
